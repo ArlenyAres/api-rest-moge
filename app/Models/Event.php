22667b5cdc7d152633950627115af84862e1221b
01joanna <joa.cuevas9@gmail.com>
2024-03-13 17:48:47 +0100

    added connections to model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
+use App\Models\Registration;
+use App\Models\Category;
 
 class Event extends Model
 {
