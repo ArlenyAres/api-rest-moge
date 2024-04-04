801d9c5d8b6bacd320a54b27ada23b99f70dbd28
01joanna <joa.cuevas9@gmail.com>
2024-04-04 12:18:20 +0200

    errores

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,57 +29,12 @@ class UserController extends Controller
         $data->save();
         return response()->json(['message' => 'User information updated successfully', 'data' => $data], 200);
 
-    }
-
-
-    public function updateProfile(Request $request)
-{
-    $user = $request->user(); // Obtener el usuario autenticado
-
-    $validator = Validator::make($request->all(), [
-        'name' => 'nullable|string|max:250',
-        'email' => 'nullable|string|email:rfc,dns|max:250|unique:users,email,',
-        'password' => 'nullable|string|min:8|confirmed',
-        'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:5000',
-    ]);
-
-    if ($validator->fails()) {
-        return response()->json(['message' => $validator->errors()->first()], 400);
-    }
-    // Guardar los datos originales del usuario antes de actualizar
-    $originalData = $user->toArray();
-
-    // Usar fill() para asignar los nuevos valores
-    $user->fill($request->only(['name', 'email']));
-
-    // Verificar si hay cambios después de asignar los nuevos valores
-    if ($user->isDirty()) {
-        if ($request->has('password')) {
-            $user->password = Hash::make($request->password);
-        }
-
-        if ($request->hasFile('image')) {
-            $imagePath = $request->file('image')->store('profile_images');
-            $user->image = $imagePath;
-        }
-
-        $user->save();
-        return response()->json(['message' => 'User profile updated successfully', 'data' => $user], 200);
-    } else {
-        // Comparar los datos originales con los datos actuales
-        if ($user->toArray() !== $originalData) {
-            $user->save();
-            return response()->json(['message' => 'User profile updated successfully', 'data' => $user], 200);
-        } else {
-            return response()->json(['message' => 'No changes detected!'], 200);
-        }
-    }
-}
-
+    }   
 
 public function getUserProfile($id)
 {
     $user = User::findOrFail($id);
+    
     
     // Construir la URL de la imagen
     $imageUrl = url($user->image_path);
