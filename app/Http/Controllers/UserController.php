befaff1545e6229150bdeca074f4df57f2c3f17c
Arleny <arleny.ares@gmail.com>
2024-04-04 23:47:27 +0200

    change validations updateProfile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,94 +23,72 @@ class UserController extends Controller
 
 
 
-    public function updateProfile(Request $request, $id)
+    public function updateProfile(Request $request)
     {
         $user = $request->user(); // Obtener el usuario autenticado
 
-        // Ajustar la regla de validación del correo electrónico para ignorar el correo del usuario actual
-        $validator = Validator::make($request->all(), [
-            'name' => 'nullable|string|max:250',
-            'email' => 'nullable|string|email:rfc,dns|max:250|unique:users,email,' . $user->id,
-            'password' => 'nullable|string|min:8|confirmed',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:5000',
-        ]);
+        // Definir las reglas de validación inicialmente vacías
+        $rules = [];
+
+        // Validar solo los campos que se proporcionan en la solicitud
+        if ($request->filled('name')) {
+            $rules['name'] = 'nullable|string|max:250';
+        }
+
+        if ($request->filled('email')) {
+            $rules['email'] = 'nullable|string|email:rfc,dns|max:250|unique:users,email,' . $user->id;
+        }
+
+        if ($request->filled('password')) {
+            $rules['password'] = 'nullable|string|min:8|confirmed';
+        }
+
+        if ($request->hasFile('image')) {
+            $rules['image'] = 'nullable|image|mimes:jpeg,png,jpg,gif|max:5000';
+        }
+
+        // Validar los campos de acuerdo a las reglas definidas
+        $validator = Validator::make($request->all(), $rules);
 
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()->first()], 400);
         }
 
-        // Guardar los datos originales del usuario antes de actualizar
-        $originalData = $user->toArray();
-
         // Usar fill() para asignar los nuevos valores
         $user->fill($request->only(['name', 'email']));
 
-        // Verificar si hay cambios después de asignar los nuevos valores
-        if ($user->isDirty()) {
-            if ($request->has('password')) {
-                $user->password = Hash::make($request->password);
-            }
-
-            // Actualizar la imagen del usuario si se proporciona una nueva imagen
-            if ($request->hasFile('image')) {
-                // Obtener el archivo y su extensión
-                $image = $request->file('image');
-                $imageName = time() . '.' . $image->getClientOriginalExtension();
-
-                // Almacenar la imagen y obtener la ruta
-                $imagePath = $image->storeAs('images/users', $imageName, 'public');
-
-                // Actualizar el path de la imagen en el modelo de usuario
-                $user->image_path = $imagePath;
-            }
-
-            $user->save();
-
-            // Construir la URL de la imagen
-            $imageUrl = $user->image_path ? url("storage/images/users/$user->image_path") : null;
-
-            // Agregar la URL de la imagen al objeto de usuario
-            $user->image_url = $imageUrl;
-
-            return response()->json(['message' => 'User profile updated successfully', 'data' => $user], 200);
-        } else {
-            // Comparar los datos originales con los datos actuales
-            if ($user->toArray() !== $originalData) {
-                $user->save();
-                return response()->json(['message' => 'User profile updated successfully', 'data' => $user], 200);
-            } else {
-                return response()->json(['message' => 'No changes detected!'], 200);
-            }
+        // Verificar si se proporcionó una nueva contraseña
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
         }
+
+        // Verificar si se proporcionó una nueva imagen
+        if ($request->hasFile('image')) {
+            // Obtener el archivo y su extensión
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+
+            // Almacenar la imagen y obtener la ruta
+            $imagePath = $image->storeAs('images/users', $imageName, 'public');
+
+            // Actualizar el path de la imagen en el modelo de usuario
+            $user->image_path = $imagePath;
+        }
+
+        // Guardar los cambios en el usuario
+        $user->save();
+
+        // Construir la URL de la imagen
+        $imageUrl = $user->image_path ? url("storage/images/users/$user->image_path") : null;
+
+        // Agregar la URL de la imagen al objeto de usuario
+        $user->image_url = $imageUrl;
+
+        return response()->json(['message' => 'User profile updated successfully', 'data' => $user], 200);
     }
 
 
 
-
-    // public function updateUserInfo(Request $request, $id)
-    // {
-    //     $data = User::findOrFail($id);
-    //     $data->fill($request->all());
-    
-    //     // Actualizar la imagen del usuario si se proporciona una nueva imagen
-    //     if ($request->hasFile('image')) {
-    //         $image = $request->file('image');
-    //         $imageName = time() . '.' . $image->extension();
-    //         $imagePath = $image->storeAs('images/users', $imageName, 'public');
-    //         $data->image_path = $imagePath;
-    //     }
-    
-    //     $data->save();
-    
-    //     // Construir la URL de la imagen
-    //     $imageUrl = $data->image_path ? url("storage/images/users/$data->image_path") : null;
-    
-    //     // Agregar la URL de la imagen al objeto de usuario
-    //     $data->image_url = $imageUrl;
-    
-    //     // Retornar la respuesta JSON con la URL de la imagen
-    //     return response()->json(['message' => 'User information updated successfully', 'data' => $data], 200);
-    // }
 
     public function getUserProfile($id)
     {
